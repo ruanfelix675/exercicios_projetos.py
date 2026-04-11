16d890c8ffea3f7a452cc97bc4716680ade9296d
ruan_void7777 <ruanfeliz7777@gmail.com>
2026-04-11 10:30:47 -0300

    Create Exercício Python 008 curso em video.py

---
+++ basico/Exercício Python 008 curso em video.py
@@ -0,0 +1,5 @@
+'''Exercício Python 008: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+'''
+
+n = float(input('digite um valor:'))
+print(f'a media de {n} corresponde a {n*100}cm e {n*100}mm')
